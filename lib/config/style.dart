3ecbdc427500d2fda3cabdfcfb180baa8e8e43c7
Thilo Jaeggi <thilo0123@gmail.com>
2022-08-02 22:20:18 +0200

    Fix Theme

--- lib/config/style.dart
+++ lib/config/style.dart
@@ -8,7 +8,6 @@ class Styles {
         useMaterial3: false,
         fontFamily: "WorkSans",
         primaryColor: isDarkTheme ? Colors.black : Colors.white,
-        backgroundColor: isDarkTheme ? Colors.black : Color(0xffF1F5FB),
         indicatorColor: isDarkTheme ? Color(0xff0E1D36) : Color(0xffCBDCF8),
         hintColor: isDarkTheme ? Color(0xff280C0B) : Color(0xffEECED3),
         focusColor: isDarkTheme ? Color(0xff0B2512) : Color(0xffA8DAB5),
@@ -21,10 +20,6 @@ class Styles {
                 isDarkTheme ? ColorScheme.dark() : ColorScheme.light()),
         appBarTheme: AppBarTheme(
           elevation: 0.0,
-        ),
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue)
-            .copyWith(
-                secondary: Colors.blue[400],
-                brightness: isDarkTheme ? Brightness.dark : Brightness.light));
+        ));
   }
 }
