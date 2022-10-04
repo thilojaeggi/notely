8c602726b5925a6fc3a69c559d24622cb77fa379
Thilo Jaeggi <thilo0123@gmail.com>
2022-10-05 00:43:59 +0200

    Add list to Globals

--- lib/Globals.dart
+++ lib/Globals.dart
@@ -1,6 +1,9 @@
+import 'Models/Grade.dart';
+
 String apiBase = "https://kaschuso.so.ch/public/";
 String accessToken = "";
 String gradeList = "";
 bool debug = false;
 bool isDark = true;
 bool hasDynamicIsland = false;
+List<Grade> globalGradeList = List.empty(growable: true);
