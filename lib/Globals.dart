4aef5ac7c72866f99d401086fce6bf34fd5b44e6
Thilo Jaeggi <thilo0123@gmail.com>
2022-10-06 00:55:17 +0200

    Add new Globals

--- lib/Globals.dart
+++ lib/Globals.dart
@@ -1,3 +1,5 @@
+import 'package:notely/Models/Exam.dart';
+
 import 'Models/Grade.dart';
 
 String apiBase = "https://kaschuso.so.ch/public/";
@@ -7,3 +9,5 @@ bool debug = false;
 bool isDark = true;
 bool hasDynamicIsland = false;
 List<Grade> globalGradeList = List.empty(growable: true);
+List<Exam> globalExamsList = List.empty(growable: true);
+int upcomingExams = 0;
