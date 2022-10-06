c00683a057d255c767a31e4f6c56bb7bd39d23c8
Thilo Jaeggi <thilo0123@gmail.com>
2022-10-06 18:57:37 +0200

    Remove gradeList

--- lib/Globals.dart
+++ lib/Globals.dart
@@ -4,7 +4,6 @@ import 'Models/Grade.dart';
 
 String apiBase = "https://kaschuso.so.ch/public/";
 String accessToken = "";
-String gradeList = "";
 bool debug = false;
 bool isDark = true;
 bool hasDynamicIsland = false;
