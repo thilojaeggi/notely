dc6491732efa85418e2f2086fb4923d9204b8b74
Thilo Jaeggi <thilo0123@gmail.com>
2023-03-10 00:10:59 +0100

    Ignore dynamci island (useless feature )

--- lib/Globals.dart
+++ lib/Globals.dart
@@ -3,10 +3,10 @@ import 'package:notely/Models/Exam.dart';
 import 'Models/Grade.dart';
 
 String apiBase = "https://kaschuso.so.ch/public/";
+String school = "";
 String accessToken = "";
 bool debug = false;
 bool isDark = true;
-bool hasDynamicIsland = false;
 List<Grade> globalGradeList = List.empty(growable: true);
 List<Exam> globalExamsList = List.empty(growable: true);
 int upcomingExams = 0;
