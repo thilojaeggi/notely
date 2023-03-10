8e84b653ddd653c3f12f864aa1f6e86b8f283f1d
Thilo Jaeggi <thilo0123@gmail.com>
2023-03-10 23:23:42 +0100

    Make list expanded

--- lib/pages/timetable_page.dart
+++ lib/pages/timetable_page.dart
@@ -41,13 +41,13 @@ class _TimetablePageState extends State<TimetablePage> {
     currentTime = now.difference(startOfDay).inMinutes / totalDuration;
   }
 
-double calculateItemHeight(DateTime startTime, DateTime endTime,
-    DateTime minStartTime, DateTime maxEndTime, double minHeight) {
-  final itemDuration = endTime.difference(startTime).inMinutes;
-  final dayDuration = maxEndTime.difference(minStartTime).inMinutes;
-  final itemHeight = itemDuration / dayDuration;
-  return max(itemHeight * minHeight, minHeight);
-}
+  double calculateItemHeight(DateTime startTime, DateTime endTime,
+      DateTime minStartTime, DateTime maxEndTime, double minHeight) {
+    final itemDuration = endTime.difference(startTime).inMinutes;
+    final dayDuration = maxEndTime.difference(minStartTime).inMinutes;
+    final itemHeight = itemDuration / dayDuration;
+    return max(itemHeight * minHeight, minHeight);
+  }
 
   void getData() async {
     final prefs = await SharedPreferences.getInstance();
@@ -132,136 +132,146 @@ double calculateItemHeight(DateTime startTime, DateTime endTime,
           child: (_eventList.length != 0)
               ? Stack(
                   children: [
-                    ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: _eventList.length,
-                        itemBuilder: (BuildContext ctxt, int index) {
-                          Event event = _eventList[index];
-                          return LayoutBuilder(builder: (BuildContext context,
-                              BoxConstraints constraints) {
-                                
-                            return Card(
-                              elevation: 3,
-                              margin: const EdgeInsets.only(
-                                  top: 5, bottom: 5, left: 10.0, right: 10.0),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              clipBehavior: Clip.antiAlias,
-                              child: InkWell(
-                                onTap: () {
-                                  if (Globals.debug) {
-                                    showToast(
-                                      alignment: Alignment.bottomCenter,
-                                      child: Container(
-                                        margin: EdgeInsets.only(bottom: 32.0),
-                                        decoration: BoxDecoration(
-                                          color: Colors.greenAccent,
-                                          borderRadius: BorderRadius.all(
-                                            Radius.circular(12.0),
+                    Expanded(
+                      child: ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: _eventList.length,
+                          itemBuilder: (BuildContext ctxt, int index) {
+                            Event event = _eventList[index];
+                            return LayoutBuilder(builder: (BuildContext context,
+                                BoxConstraints constraints) {
+                              return Card(
+                                elevation: 3,
+                                margin: const EdgeInsets.only(
+                                    top: 5, bottom: 5, left: 10.0, right: 10.0),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                clipBehavior: Clip.antiAlias,
+                                child: InkWell(
+                                  onTap: () {
+                                    if (Globals.debug) {
+                                      showToast(
+                                        alignment: Alignment.bottomCenter,
+                                        child: Container(
+                                          margin: EdgeInsets.only(bottom: 32.0),
+                                          decoration: BoxDecoration(
+                                            color: Colors.greenAccent,
+                                            borderRadius: BorderRadius.all(
+                                              Radius.circular(12.0),
+                                            ),
+                                          ),
+                                          padding: EdgeInsets.all(6.0),
+                                          child: Text(
+                                            "Tapped",
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 16.0,
+                                            ),
                                           ),
                                         ),
-                                        padding: EdgeInsets.all(6.0),
-                                        child: Text(
-                                          "Tapped",
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 16.0,
+                                        context: context,
+                                      );
+                                    }
+                                  },
+                                  child: Container(
+                                      padding: EdgeInsets.only(
+                                          left: 7.0,
+                                          right: 7.0,
+                                          top: 2.0,
+                                          bottom: 2.0),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          Expanded(
+                                            child: Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  event.courseName.toString(),
+                                                  textAlign: TextAlign.start,
+                                                  style: const TextStyle(
+                                                      fontSize: 23,
+                                                      height: 1.1,
+                                                      fontWeight:
+                                                          FontWeight.w600),
+                                                ),
+                                                Text(
+                                                  event.teachers!.first
+                                                      .toString(),
+                                                  textAlign: TextAlign.start,
+                                                  style: const TextStyle(
+                                                    fontSize: 17,
+                                                    height: 1.2,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
                                           ),
-                                        ),
-                                      ),
-                                      context: context,
-                                    );
-                                  }
-                                },
-                                child: Container(
-                                    padding: EdgeInsets.only(
-                                        left: 7.0,
-                                        right: 7.0,
-                                        top: 2.0,
-                                        bottom: 2.0),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.center,
-                                      children: [
-                                        Expanded(
-                                          child: Column(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
+                                          SizedBox(
+                                            width: 5,
+                                          ),
+                                          Text(
+                                            event.roomToken.toString(),
+                                            style: const TextStyle(
+                                                fontSize: 24,
+                                                fontWeight: FontWeight.w500),
+                                          ),
+                                          SizedBox(
+                                            width: 5,
+                                          ),
+                                          Column(
                                             children: [
                                               Text(
-                                                event.courseName.toString(),
-                                                textAlign: TextAlign.start,
-                                                style: const TextStyle(
-                                                    fontSize: 23,
-                                                    fontWeight:
-                                                        FontWeight.w600),
+                                                event.startDate!.substring(
+                                                    event.startDate!.length -
+                                                        5),
+                                                style:
+                                                    TextStyle(fontSize: 18.0),
+                                                textAlign: TextAlign.center,
                                               ),
                                               Text(
-                                                event.teachers!.first
+                                                "-",
+                                                style:
+                                                    TextStyle(fontSize: 18.0),
+                                                textAlign: TextAlign.center,
+                                              ),
+                                              Text(
+                                                event.endDate!
+                                                    .substring(event
+                                                            .startDate!.length -
+                                                        5)
                                                     .toString(),
-                                                textAlign: TextAlign.start,
-                                                style: const TextStyle(
-                                                  fontSize: 17,
-                                                  height: 1,
+                                                style: TextStyle(
+                                                  fontSize: 18.0,
                                                 ),
+                                                textAlign: TextAlign.center,
                                               ),
                                             ],
                                           ),
-                                        ),
-                                        SizedBox(
-                                          width: 5,
-                                        ),
-                                        Text(
-                                          event.roomToken.toString(),
-                                          style: const TextStyle(
-                                              fontSize: 24,
-                                              fontWeight: FontWeight.w500),
-                                        ),
-                                        SizedBox(
-                                          width: 5,
-                                        ),
-                                        Column(
-                                          children: [
-                                            Text(
-                                              event.startDate!.substring(
-                                                  event.startDate!.length - 5),
-                                              style: TextStyle(fontSize: 18.0),
-                                              textAlign: TextAlign.center,
-                                            ),
-                                            Text(
-                                              "-",
-                                              style: TextStyle(fontSize: 18.0),
-                                              textAlign: TextAlign.center,
-                                            ),
-                                            Text(
-                                              event.endDate!
-                                                  .substring(
-                                                      event.startDate!.length -
-                                                          5)
-                                                  .toString(),
-                                              style: TextStyle(
-                                                fontSize: 18.0,
-                                              ),
-                                              textAlign: TextAlign.center,
-                                            ),
-                                          ],
-                                        ),
-                                      ],
-                                    )),
-                              ),
-                            );
-                          });
-                        }),
+                                        ],
+                                      )),
+                                ),
+                              );
+                            });
+                          }),
+                    ),
                     Positioned(
                       top: currentTime * 200,
                       left: 0,
-                      right: 0,
                       child: Container(
-                        height: 1,
-                        color: Colors.red,
+                        height: 5,
+                        width: 200,
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.only(
+                              bottomRight: Radius.circular(12.0),
+                              topRight: Radius.circular(12.0),
+                            )),
                       ),
                     ),
                   ],
