1b0d6e98d9ad9abde49cef243f11adf2e3943320
Thilo Jaeggi <thilo0123@gmail.com>
2023-03-12 23:08:19 +0100

    Formatting and change text if no homework

--- lib/pages/homework_page.dart
+++ lib/pages/homework_page.dart
@@ -1,11 +1,7 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:notely/Models/Homework.dart';
 import 'package:notely/helpers/HomeworkDatabase.dart';
-import '../Globals.dart' as Globals;
-import '../Models/Exam.dart';
 
 class HomeworkPage extends StatefulWidget {
   const HomeworkPage({Key? key, required this.homeworkList}) : super(key: key);
@@ -61,121 +57,158 @@ class _HomeworkPageState extends State<HomeworkPage> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: homeworkList.length,
-              itemBuilder: (BuildContext ctxt, int index) {
-                Homework homework = homeworkList[index];
-                return Card(
-                  elevation: 3,
-                  margin: const EdgeInsets.only(
-                      bottom: 10, left: 10.0, right: 10.0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  clipBehavior: Clip.antiAlias,
-                  child: Container(
-                    padding: EdgeInsets.all(10.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Expanded(
-                              child: FittedBox(
-                                fit: BoxFit.scaleDown,
-                                alignment: Alignment.topLeft,
-                                child: Text(
-                                  homeworkList[index].lessonName.toString(),
-                                  style: const TextStyle(
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.w500,
+              child: (homeworkList.isNotEmpty)
+                  ? ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: homeworkList.length,
+                      itemBuilder: (BuildContext ctxt, int index) {
+                        Homework homework = homeworkList[index];
+                        return Card(
+                          elevation: 3,
+                          margin: const EdgeInsets.only(
+                              bottom: 10, left: 10.0, right: 10.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          clipBehavior: Clip.antiAlias,
+                          child: Container(
+                            padding: EdgeInsets.all(10.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    Expanded(
+                                      child: FittedBox(
+                                        fit: BoxFit.scaleDown,
+                                        alignment: Alignment.topLeft,
+                                        child: Text(
+                                          homeworkList[index]
+                                              .lessonName
+                                              .toString(),
+                                          style: const TextStyle(
+                                            fontSize: 24,
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 20,
+                                    ),
+                                    Text(
+                                      DateFormat("dd.MM.yyyy").format(
+                                          DateTime.parse(homeworkList[index]
+                                              .dueDate
+                                              .toString())),
+                                      style: const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Container(
+                                    margin: const EdgeInsets.only(bottom: 15.0),
+                                    height: 2.0,
+                                    width: 100.0,
+                                    decoration: BoxDecoration(
+                                      color: Theme.of(context).brightness ==
+                                              Brightness.dark
+                                          ? Colors.white
+                                          : Colors.black,
+                                      borderRadius: BorderRadius.circular(8.0),
+                                    )),
+                                FittedBox(
+                                  fit: BoxFit.scaleDown,
+                                  alignment: Alignment.topLeft,
+                                  child: Text(
+                                    homeworkList[index].title.toString(),
+                                    style: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w500,
+                                      height: 1.0,
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ),
-                            SizedBox(
-                              width: 20,
-                            ),
-                            Text(
-                              DateFormat("dd.MM.yyyy").format(DateTime.parse(
-                                  homeworkList[index].dueDate.toString())),
-                              style: const TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Container(
-                            margin: const EdgeInsets.only(bottom: 15.0),
-                            height: 2.0,
-                            width: 100.0,
-                            decoration: BoxDecoration(
-                              color: Theme.of(context).brightness ==
-                                      Brightness.dark
-                                  ? Colors.white
-                                  : Colors.black,
-                              borderRadius: BorderRadius.circular(8.0),
-                            )),
-                        FittedBox(
-                          fit: BoxFit.scaleDown,
-                          alignment: Alignment.topLeft,
-                          child: Text(
-                            homeworkList[index].title.toString(),
-                            style: const TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w500,
-                              height: 1.0,
+                                Row(
+                                  children: [
+                                    Expanded(
+                                      child: FittedBox(
+                                        fit: BoxFit.scaleDown,
+                                        alignment: Alignment.topLeft,
+                                        child: Text(
+                                          homeworkList[index]
+                                              .details
+                                              .toString(),
+                                          style: const TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.w500),
+                                        ),
+                                      ),
+                                    ),
+                                    Align(
+                                      alignment: Alignment.bottomRight,
+                                      child: Transform.scale(
+                                        scale: 1.7,
+                                        child: Checkbox(
+                                            value: homework.isDone,
+                                            fillColor:
+                                                MaterialStateProperty.all(
+                                                    Theme.of(context)
+                                                        .primaryColor),
+                                            // Rounded checkbox
+                                            shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(4),
+                                            ),
+                                            onChanged: (newVal) async {
+                                              Homework updatedHomework =
+                                                  homework.copyWith(
+                                                      isDone: newVal!);
+                                              await HomeworkDatabase.instance
+                                                  .update(updatedHomework);
+                                              setState(() {
+                                                homeworkList[index] =
+                                                    updatedHomework;
+                                              });
+                                            }),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
                             ),
                           ),
-                        ),
-                        Row(
-                          children: [
-                            Expanded(
-                              child: FittedBox(
-                                fit: BoxFit.scaleDown,
-                                alignment: Alignment.topLeft,
-                                child: Text(
-                                  homeworkList[index].description.toString(),
-                                  style: const TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w500),
-                                ),
-                              ),
+                        );
+                      },
+                    )
+                  : Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Keine Hausaufgaben vorhanden, geniesse den Tag!",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w500,
                             ),
-                            Align(
-                              alignment: Alignment.bottomRight,
-                              child: Transform.scale(
-                                scale: 1.7,
-                                child: Checkbox(
-                                    value: homework.isDone,
-                                    fillColor: MaterialStateProperty.all(
-                                        Theme.of(context).primaryColor),
-                                    // Rounded checkbox
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(4),
-                                    ),
-                                    onChanged: (newVal) async {
-                                      Homework updatedHomework =
-                                          homework.copyWith(isDone: newVal!);
-                                      await HomeworkDatabase.instance
-                                          .update(updatedHomework);
-                                      setState(() {
-                                        homeworkList[index] = updatedHomework;
-                                      });
-                                    }),
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                              "Hausaufgaben kannst du hinzufügen, indem du auf eine Lektion unter \"Plan\" tippst.",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
                               ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
+                              textAlign: TextAlign.center),
+                        ],
+                      ),
+                    )),
         ],
       ),
     );
