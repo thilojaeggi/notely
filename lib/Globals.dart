1f36f3c407378438df297f72ac5dcd404d46ef79
Thilo Jaeggi <thilo0123@gmail.com>
2023-03-24 11:40:16 +0100

    Make Globals singleton

--- lib/Globals.dart
+++ lib/Globals.dart
@@ -1,8 +1,18 @@
 import 'package:notely/Models/Exam.dart';
 
-String apiBase = "https://kaschuso.so.ch/public/";
-String school = "";
-String accessToken = "";
-bool debug = false;
-bool isDark = true;
-List<Exam> globalExamsList = List.empty(growable: true);
+class Globals {
+  static final Globals _singleton = Globals._internal();
+
+  factory Globals() {
+    return _singleton;
+  }
+
+  Globals._internal();
+
+  String apiBase = "https://kaschuso.so.ch/public/";
+  String school = "";
+  String accessToken = "";
+  bool debug = false;
+  bool isDark = true;
+  List<Exam> globalExamsList = List.empty(growable: true);
+}
