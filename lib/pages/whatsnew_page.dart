b221c7898ebc05f9f670d43749064f10a0042130
Thilo Jaeggi <thilo0123@gmail.com>
2023-03-27 16:05:59 +0200

    Add whats new

--- lib/pages/whatsnew_page.dart
+++ lib/pages/whatsnew_page.dart
@@ -57,6 +57,29 @@ class _WhatsNewState extends State<WhatsNew> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Icon(
+                        FontAwesome5.tachometer_alt,
+                        size: 32,
+                        color: Colors.blue.shade500,
+                      ),
+                    ],
+                  ),
+                  title: Text(
+                    'Ladezeiten verringert',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                  ), //Title is the only Required Item
+                  subtitle: Text(
+                    'Alle Daten werden nun schneller geladen und es sollten keine Ladekreise mehr zu sehen sein.',
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                )
+                ,
+                ListTile(
+                  leading: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
                         FontAwesome5.pen_nib,
                         size: 32,
                         color: Colors.blue.shade500,
@@ -69,6 +92,28 @@ class _WhatsNewState extends State<WhatsNew> {
                   ), //Title is the only Required Item
                   subtitle: Text(
                     'Bei den neuesten Noten werden nun Testnamen grösser angezeigt.',
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                ListTile(
+                  leading: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        FontAwesome5.plus,
+                        size: 32,
+                        color: Colors.blue.shade500,
+                      ),
+                    ],
+                  ),
+                  title: Text(
+                    'Hausaufgaben manuell hinzufügen',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                  ), //Title is the only Required Item
+                  subtitle: Text(
+                    'Hausaufgaben können nun auch manuell hinzugefügt werden auch ausserhalb des Stundenplans.\nZudem wurde die Darstellung überarbeitet.',
                   ),
                 ),
                 SizedBox(
@@ -97,7 +142,7 @@ class _WhatsNewState extends State<WhatsNew> {
                           'Fehlerbehebung Startseite',
                           style: TextStyle(
                               fontSize: 16, fontWeight: FontWeight.w600),
-                        ), 
+                        ),
                         subtitle: Text(
                           'Auf der Startseite wurden fälschlicherweise Noten von alt nach neu dargestellt. ',
                           style: TextStyle(fontSize: 13.0),
@@ -121,6 +166,27 @@ class _WhatsNewState extends State<WhatsNew> {
                         ), //Title is the only Required Item
                         subtitle: Text(
                           'Bei einigen Usern war es möglich dass man nicht angemeldet bleibt, dies sollte nun behoben sein.',
+                          style: TextStyle(fontSize: 13.0),
+                        ),
+                      ),
+                      ListTile(
+                        leading: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              FontAwesome5.bug,
+                              size: 24,
+                              color: Colors.blue.shade500,
+                            ),
+                          ],
+                        ),
+                        title: Text(
+                          'Fehlerbehebung Hausaufgaben',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w600),
+                        ), //Title is the only Required Item
+                        subtitle: Text(
+                          'Zeilenumbrüche in Hausaufgaben sollten nun korrekt dargestellt werden.',
                           style: TextStyle(fontSize: 13.0),
                         ),
                       ),
