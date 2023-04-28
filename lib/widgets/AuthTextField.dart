8be4b6dd96179fce8cde51cf598c5d1d980a092e
Thilo Jaeggi <thilo0123@gmail.com>
2023-04-28 16:40:41 +0200

    Change text size of auth fields and remove border

--- lib/widgets/AuthTextField.dart
+++ lib/widgets/AuthTextField.dart
@@ -19,38 +19,27 @@ class AuthTextField extends StatelessWidget {
     return Card(
       color: Colors.transparent,
       elevation: 0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16),
-      ),
       child: TextFormField(
         textInputAction: textInputAction,
         controller: editingController,
         style: const TextStyle(
           color: Colors.white,
+          fontSize: 18.0,
         ),
         obscureText: passwordField,
         enableSuggestions: !passwordField,
         autocorrect: false,
         maxLines: 1,
         decoration: InputDecoration(
-          hintStyle: const TextStyle(color: Colors.white),
+          hintStyle:  TextStyle(color: Colors.white.withOpacity(0.8)),
           hintText: hintText,
           prefixIcon: Icon(
             icon,
             color: Colors.white,
           ),
-          enabledBorder: const OutlineInputBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(16),
-            ),
-            borderSide: BorderSide(color: Colors.white, width: 2.5),
-          ),
-          focusedBorder: const OutlineInputBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(16),
-            ),
-            borderSide: BorderSide(color: Colors.white, width: 4.0),
-          ),
+          
+          border: InputBorder.none,
+
         ),
       ),
     );
