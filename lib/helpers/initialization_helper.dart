5e296896ffcb48c0f5c5ae53374f803f247bd4fb
Thilo <thilo0123@gmail.com>
2023-10-02 13:53:03 +0200

    Fix initalization helper

--- lib/helpers/initialization_helper.dart
+++ lib/helpers/initialization_helper.dart
@@ -3,7 +3,28 @@ import 'dart:async';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class InitializationHelper {
-  Future<void> initialize() async {
+  Future<FormError?> initialize() async {
+    final completer = Completer<FormError?>();
+
+    final params = ConsentRequestParameters();
+    ConsentInformation.instance.requestConsentInfoUpdate(params, () async {
+      if (await ConsentInformation.instance.isConsentFormAvailable()) {
+        await _loadConsentForm();
+      } else {
+        // There is no message to display,
+        // so initialize the components here.
+        await _initialize();
+      }
+
+      completer.complete();
+    }, (error) {
+      completer.complete(error);
+    });
+
+    return completer.future;
+  }
+
+  Future<void> _initialize() async {
     await MobileAds.instance.initialize();
 
     /**
@@ -26,7 +47,7 @@ class InitializationHelper {
       } else {
         // The user has chosen an option,
         // it's time to initialize the ads component.
-        await initialize();
+        await _initialize();
         completer.complete();
       }
     }, (FormError? error) {
@@ -39,18 +60,12 @@ class InitializationHelper {
   Future<bool> changePrivacyPreferences() async {
     final completer = Completer<bool>();
 
-    final params = ConsentRequestParameters(
-      consentDebugSettings: ConsentDebugSettings(
-        debugGeography: DebugGeography.debugGeographyEea,
-        // Or DebugGeography.debugGeographyNotEea to simulate outside of the EEA
-      ),
-    );
-
-    ConsentInformation.instance.requestConsentInfoUpdate(params, () async {
+    ConsentInformation.instance
+        .requestConsentInfoUpdate(ConsentRequestParameters(), () async {
       if (await ConsentInformation.instance.isConsentFormAvailable()) {
         ConsentForm.loadConsentForm((consentForm) {
           consentForm.show((formError) async {
-            await initialize();
+            await _initialize();
             completer.complete(true);
           });
         }, (formError) {
