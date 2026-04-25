091ed726f0b206a76f9ee331f75b2cd1253fa39f
Thilo <thilo0123@gmail.com>
2026-04-25 14:56:01 +0200

    Improve data models

--- lib/models/event.dart
+++ lib/models/event.dart
@@ -1,111 +1,96 @@
-// ignoreforfile: prefervoidtonull
-
-// ignore_for_file: prefer_void_to_null
-
 class Event {
-  String? id;
-  DateTime? startDate;
-  DateTime? endDate;
-  String? text;
-  String? comment;
-  String? roomToken;
-  String? roomId;
-  List<String>? teachers;
-  List<String>? teacherIds;
-  List<String>? teacherTokens;
-  String? courseId;
-  String? courseToken;
-  String? courseName;
-  String? status;
-  String? color;
-  String? eventType;
-  bool? isExam;
-  Null eventRoomStatus;
-  Null timetableText;
-  Null infoFacilityManagement;
-  Null importset;
-  Null lessons;
-  Null publishToInfoSystem;
-  Null studentNames;
-  Null studentIds;
+  final String? id;
+  final DateTime? startDate;
+  final DateTime? endDate;
+  final String? text;
+  final String? comment;
+  final String? roomToken;
+  final String? roomId;
+  final List<String>? teachers;
+  final List<String>? teacherIds;
+  final List<String>? teacherTokens;
+  final String? courseId;
+  final String? courseToken;
+  final String? courseName;
+  final String? status;
+  final String? color;
+  final String? eventType;
+  final String? eventRoomStatus;
+  final dynamic timetableText;
+  final dynamic infoFacilityManagement;
+  final dynamic importset;
+  final dynamic lessons;
+  final dynamic publishToInfoSystem;
+  final List<String>? studentNames;
+  final List<String>? studentIds;
+  bool isExam;
 
-  Event(
-      {String? id,
-      DateTime? startDate,
-      DateTime? endDate,
-      String? text,
-      String? comment,
-      String? roomToken,
-      String? roomId,
-      List<String>? teachers,
-      List<String>? teacherIds,
-      List<String>? teacherTokens,
-      String? courseId,
-      String? courseToken,
-      String? courseName,
-      String? status,
-      String? color,
-      String? eventType,
-      bool? isExam,
-      Null eventRoomStatus,
-      Null timetableText,
-      Null infoFacilityManagement,
-      Null importset,
-      Null lessons,
-      Null publishToInfoSystem,
-      Null studentNames,
-      Null studentIds}) {
-    this.id = id;
-    this.startDate = startDate;
-    this.endDate = endDate;
-    this.text = text;
-    this.comment = comment;
-    this.roomToken = roomToken;
-    this.roomId = roomId;
-    this.teachers = teachers;
-    this.teacherIds = teacherIds;
-    this.teacherTokens = teacherTokens;
-    this.courseId = courseId;
-    this.courseToken = courseToken;
-    this.courseName = courseName;
-    this.status = status;
-    this.color = color;
-    this.eventType = eventType;
-    this.isExam = isExam;
-  }
+  Event({
+    this.id,
+    this.startDate,
+    this.endDate,
+    this.text,
+    this.comment,
+    this.roomToken,
+    this.roomId,
+    this.teachers,
+    this.teacherIds,
+    this.teacherTokens,
+    this.courseId,
+    this.courseToken,
+    this.courseName,
+    this.status,
+    this.color,
+    this.eventType,
+    this.eventRoomStatus,
+    this.timetableText,
+    this.infoFacilityManagement,
+    this.importset,
+    this.lessons,
+    this.publishToInfoSystem,
+    this.studentNames,
+    this.studentIds,
+    this.isExam = false,
+  });
 
-  Event.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    startDate = DateTime.parse(json['startDate']);
-    endDate = DateTime.parse(json['endDate']);
-    text = json['text'];
-    comment = json['comment'];
-    roomToken = json['roomToken'];
-    roomId = json['roomId'];
-    teachers = json['teachers'].cast<String>();
-    teacherIds = json['teacherIds'].cast<String>();
-    teacherTokens = json['teacherTokens'].cast<String>();
-    courseId = json['courseId'];
-    courseToken = json['courseToken'];
-    courseName = json['courseName'];
-    status = json['status'];
-    color = json['color'];
-    eventType = json['eventType'];
-    eventRoomStatus = json['eventRoomStatus'];
-    timetableText = json['timetableText'];
-    infoFacilityManagement = json['infoFacilityManagement'];
-    importset = json['importset'];
-    lessons = json['lessons'];
-    publishToInfoSystem = json['publishToInfoSystem'];
-    studentNames = json['studentNames'];
-    studentIds = json['studentIds'];
+  factory Event.fromJson(Map<String, dynamic> json) {
+    return Event(
+      id: json['id'],
+      startDate: json['startDate'] != null
+          ? DateTime.parse(json['startDate'])
+          : null,
+      endDate: json['endDate'] != null
+          ? DateTime.parse(json['endDate'])
+          : null,
+      text: json['text'],
+      comment: json['comment'],
+      roomToken: json['roomToken'],
+      roomId: json['roomId'],
+      teachers: json['teachers'] != null ? json['teachers'].cast<String>() : null,
+      teacherIds: json['teacherIds'] != null ? json['teacherIds'].cast<String>() : null,
+      teacherTokens: json['teacherTokens'] != null ? json['teacherTokens'].cast<String>() : null,
+      courseId: json['courseId'],
+      courseToken: json['courseToken'],
+      courseName: json['courseName'],
+      status: json['status'],
+      color: json['color'],
+      eventType: json['eventType'],
+      eventRoomStatus: json['eventRoomStatus'],
+      timetableText: json['timetableText'],
+      infoFacilityManagement: json['infoFacilityManagement'],
+      importset: json['importset'],
+      lessons: json['lessons'],
+      publishToInfoSystem: json['publishToInfoSystem'],
+      studentNames: json['studentNames'] != null ? json['studentNames'].cast<String>() : null,
+      studentIds: json['studentIds'] != null ? json['studentIds'].cast<String>() : null,
+    );
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['id'] = id;
-    data['startDate'] = startDate;
-    data['endDate'] = endDate;
+    data['startDate'] = startDate?.toIso8601String();
+    data['endDate'] = endDate?.toIso8601String();
     data['text'] = text;
     data['comment'] = comment;
     data['roomToken'] = roomToken;
--- lib/models/grade.dart
+++ lib/models/grade.dart
@@ -1,57 +1,48 @@
 class Grade {
-  String? id;
-  String? course;
-  String? courseType;
-  String? subject;
-  String? subjectToken;
-  String? title;
-  DateTime? date;
-  num? mark;
-  num? weight;
-  bool? isConfirmed;
+  final String? id;
+  final String? course;
+  final String? subject;
+  final String? title;
+  final DateTime? date;
+  final num? mark;
+  final num? weight;
+  final bool isIgnored;
 
-  Grade(
+  const Grade(
       {this.id,
       this.course,
-      this.courseType,
       this.subject,
-      this.subjectToken,
       this.title,
       this.date,
       this.mark,
       this.weight,
-      this.isConfirmed});
+      this.isIgnored = false});
 
-  Grade.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    course = json['course'];
-    courseType = json['courseType'];
-    subject = json['subject'];
-    subjectToken = json['subjectToken'];
-    title = json['title'];
-    date = json['date'] != null ? DateTime.tryParse(json['date']) : null;
-    mark = json['mark'];
-    weight = json['weight'];
-    isConfirmed = json['isConfirmed'];
+  factory Grade.fromJson(Map<String, dynamic> json) {
+    if (json['id'].runtimeType == int) {
+      json['id'] = json['id'].toString();
+    }
+    return Grade(
+        id: json['id'],
+        course: json['course'],
+        subject: json['subject'],
+        title: json['title'],
+        date: json['date'] != null ? DateTime.tryParse(json['date']) : null,
+        mark: json['mark'],
+        weight: json['weight'],
+        isIgnored: json['isIgnored'] ?? false);
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['id'] = id;
     data['course'] = course;
-    data['courseType'] = courseType;
     data['subject'] = subject;
-    data['subjectToken'] = subjectToken;
     data['title'] = title;
-    data['date'] =
-        date?.toIso8601String(); // format: yyyy-MM-ddTHH:mm:ss.mmmuuu
-    // if the API expects just date, this might include time, but usually fine for ISO parsers.
-    // If stricly needed YYYY-MM-DD we can adjust.
-    // Given the user request "store as datetime", keeping precision is usually safe unless sending back to strict API.
-    // The toJson is often used for caching here (shared_preferences).
+    data['date'] = date?.toIso8601String(); // format date to iso8601
     data['mark'] = mark;
     data['weight'] = weight;
-    data['isConfirmed'] = isConfirmed;
+    data['isIgnored'] = isIgnored;
     return data;
   }
 }
