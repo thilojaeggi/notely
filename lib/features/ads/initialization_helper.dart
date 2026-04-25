f3c989ca04f10c7c87903c18d5d99b405748a4d0
Thilo <thilo0123@gmail.com>
2026-04-25 14:51:08 +0200

    Do not use debug params in prod

--- lib/features/ads/initialization_helper.dart
+++ lib/features/ads/initialization_helper.dart
@@ -13,7 +13,7 @@ class InitializationHelper {
       ),
     );
     ConsentInformation.instance
-        .requestConsentInfoUpdate(params, () async {
+        .requestConsentInfoUpdate(ConsentRequestParameters(), () async {
       if (await ConsentInformation.instance.isConsentFormAvailable()) {
         await _loadConsentForm();
       } else {
